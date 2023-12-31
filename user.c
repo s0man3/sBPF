a5ac3337dc5347791142703c1ff1641679e4b7a4
s0man3 <soma.nakata01@gmail.com>
2023-12-31 14:31:04 +0900

    modified userland programs

--- user.c
+++ user.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -15,6 +16,14 @@ int main() {
         attr->insn_len = 0x20;
         attr->insn_cnt = 0x20 / 0x8;
         ret = syscall(548, attr, sizeof(attr));
+
+        printf("Argument:\n"
+               "  attr->insns = %llx/n"
+               "  attr->insn_len = %d"
+               "  attr->insn_cnt = %d"
+               " Return value: %d"
+               attr->insns, attr->insns_len,
+               attr->insn_cnt, ret);
 
         free(attr);
         free(stuff);
